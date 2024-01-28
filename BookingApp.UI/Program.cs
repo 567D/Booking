e7af3ae4d72b8650589d05e738feeca56628ae0e
dkononenko <daria.emerald8@gmail.com>
2024-01-28 10:03:44 +0300

    console command handler

--- BookingApp.UI/Program.cs
+++ BookingApp.UI/Program.cs
@@ -3,6 +3,13 @@ using BookingApp.Entities.Models;
 
 public class Program
 {
+    private static void ShowBookings(List<Booking> bookings)
+    {
+        foreach (var booking in bookings)
+        {
+            Console.WriteLine($"User with id = {booking.UserId} has booked room with id = {booking.RoomNum}");
+        }
+    }
     public static void Main(string[] args)
     {
         //define variables
@@ -14,28 +21,55 @@ public class Program
         //create service class object
         BookingService service = new BookingService();
 
-        //book room with number = 1 by user with id = 1
-        Console.WriteLine(service.Book(userId, room1Id));
-        //book room with number = 2 by user with id = 1
-        Console.WriteLine(service.Book(userId, room2Id));
-        //book room with number = 3 by user with id = 1
-        Console.WriteLine(service.Book(userId, room3Id));
+        
 
-        service.CancelBooking(userId, room1Id);
-        service.CancelBooking(userId, room3Id);
+        //service.CancelBooking(userId, room1Id);
+        //service.CancelBooking(userId, room3Id);
 
         //put empty line delimiter
-        Console.WriteLine("_________________");
+        //Console.WriteLine("_________________");
 
         //get bookings by user id = 1
         List<Booking> bookings = service.GetBookings(userId);
 
         //show bookings from user with id = 1
-        foreach (var booking in bookings)
+        
+        //loop request
+        bool ExitRequest = false;
+        while (ExitRequest == false)
         {
-            Console.WriteLine($"User with id = {booking.UserId} has booked room with id = { booking.RoomNum }");
-        }
+            Console.WriteLine("Enter a command (create, get, cancel, exit):");
+            string UserInput = Console.ReadLine();
 
+            if (UserInput == "create")
+            {
+                //book room with number = 1 by user with id = 1
+                Console.WriteLine(service.Book(userId, room1Id));
+                //book room with number = 2 by user with id = 1
+                Console.WriteLine(service.Book(userId, room2Id));
+                //book room with number = 3 by user with id = 1
+                Console.WriteLine(service.Book(userId, room3Id));
+            }
+            if (UserInput == "get")
+            {
+                bookings = service.GetBookings(userId);
+                ShowBookings(bookings);
+            }
+            if (UserInput == "cancel")
+            {
+                service.CancelBooking(userId, room1Id);
+                service.CancelBooking(userId, room3Id);
+
+                //put empty line delimiter
+                Console.WriteLine("_________________");
+
+            }
+            if (UserInput == "exit")
+            {
+                ExitRequest = true;
+                Console.WriteLine("Exit");
+            }
+        }
         //Success message, program has finished successfully
         Console.WriteLine("The program has finished!!!");
     }
