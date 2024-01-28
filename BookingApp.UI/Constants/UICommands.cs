f17d1e64dac04d70aa161b32078bc66b4b04df01
Pavel K <pavelkononenko1@gmail.com>
2024-01-28 09:01:39 +0100

    Changes after review HW3

---
+++ BookingApp.UI/Constants/UICommands.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace BookingApp.UI.Constants
+{
+	public class UICommands
+	{
+        public const string CREATE = "create";
+        public const string CANCEL = "cancel";
+        public const string GET = "get";
+        public const string EXIT = "exit";
+    }
+}
+
--- BookingApp.UI/Program.cs
+++ BookingApp.UI/Program.cs
@@ -1,15 +1,9 @@
 ﻿using BookingApp.Core.Services;
 using BookingApp.Entities.Models;
+using BookingApp.UI.Constants;
 
 public class Program
 {
-    private static void ShowBookings(List<Booking> bookings)
-    {
-        foreach (var booking in bookings)
-        {
-            Console.WriteLine($"User with id = {booking.UserId} has booked room with id = {booking.RoomNum}");
-        }
-    }
     public static void Main(string[] args)
     {
         //define variables
@@ -17,8 +11,6 @@ public class Program
         int room1Id = 1;
         int room2Id = 2;
         int room3Id = 3;
-
-
 
         //create service for bookings
         BookingService bookingService = new BookingService();
@@ -29,57 +21,66 @@ public class Program
         //creation user in real database
         //userService.AddUser(Guid.NewGuid().ToString(), "Daria");
 
-        
+        //book room with number = 1 by user with id = 1
+        Console.WriteLine(bookingService.Book(userId, room1Id));
+        //book room with number = 2 by user with id = 1
+        Console.WriteLine(bookingService.Book(userId, room2Id));
+        //book room with number = 3 by user with id = 1
+        Console.WriteLine(bookingService.Book(userId, room3Id));
 
-        //service.CancelBooking(userId, room1Id);
-        //service.CancelBooking(userId, room3Id);
+        bookingService.CancelBooking(userId, room1Id);
+        bookingService.CancelBooking(userId, room3Id);
 
         //put empty line delimiter
-        //Console.WriteLine("_________________");
+        Console.WriteLine("_________________");
+        Console.WriteLine();
 
         //get bookings by user id = 1
         List<Booking> bookings = bookingService.GetBookings(userId);
 
         //show bookings from user with id = 1
-        
-        //loop request
-        bool ExitRequest = false;
-        while (ExitRequest == false)
+        ShowBookings(bookings);
+
+        bool exit = false;
+
+        while(!exit)
         {
             Console.WriteLine("Enter a command (create, get, cancel, exit):");
-            string UserInput = Console.ReadLine();
+            Console.WriteLine();
+            string command = Console.ReadLine().ToLower();
 
-            if (UserInput == "create")
+            switch (command)
             {
-                //book room with number = 1 by user with id = 1
-                Console.WriteLine(bookingService.Book(userId, room1Id));
-                //book room with number = 2 by user with id = 1
-                Console.WriteLine(bookingService.Book(userId, room2Id));
-                //book room with number = 3 by user with id = 1
-                Console.WriteLine(bookingService.Book(userId, room3Id));
-            }
-            if (UserInput == "get")
-            {
-                bookings = bookingService.GetBookings(userId);
-                ShowBookings(bookings);
-            }
-            if (UserInput == "cancel")
-            {
-                bookingService.CancelBooking(userId, room1Id);
-                bookingService.CancelBooking(userId, room3Id);
-
-                //put empty line delimiter
-                Console.WriteLine("_________________");
-
-            }
-            if (UserInput == "exit")
-            {
-                ExitRequest = true;
-                Console.WriteLine("Exit");
+                case UICommands.GET:
+                    Console.WriteLine($"{nameof(Booking)} { UICommands.GET}");
+                    break;
+                case UICommands.CREATE:
+                    Console.WriteLine($"{nameof(Booking)} {UICommands.CREATE}");
+                    break;
+                case UICommands.CANCEL:
+                    Console.WriteLine($"{nameof(Booking)} {UICommands.CANCEL}");
+                    break;
+                case UICommands.EXIT:
+                    exit = true;
+                    Console.WriteLine($"{nameof(Booking)} {UICommands.GET}");
+                    break;
+                default:
+                    Console.WriteLine($"Unknown command, please retry with {UICommands.GET}, {UICommands.CREATE}, {UICommands.CANCEL}, {UICommands.EXIT} command");
+                    Console.WriteLine();
+                    break;
             }
         }
+
         //Success message, program has finished successfully
         Console.WriteLine("The program has finished!!!");
         Console.ReadLine();
+    }
+
+    private static void ShowBookings(List<Booking> bookings)
+    {
+        foreach (var booking in bookings)
+        {
+            Console.WriteLine($"User with id = {booking.UserId} has booked room with id = {booking.RoomNum}");
+        }
     }
 }
