86d2ce46a11e6707d0753b0e48cee366a9ed2bc9
dkononenko <daria.emerald8@gmail.com>
2024-02-09 03:19:55 +0300

    Create RoomRepository.cs in Repositories

--- BookingApp.Entities/Models/Room.cs
+++ BookingApp.Entities/Models/Room.cs
@@ -4,5 +4,10 @@
     {
         public string Id { get; set; }
         public int Num { get; set; }
+        public double SquareMeters { get; set; }
+        public int NumberOfBeds { get; set; }
+        public double PricePerNight { get; set; }
+        public int Capacity { get; set; }
+        public int Floor { get; set; }
     }
 }
---
+++ BookingApp.Persistence/Repositories/RoomRepository.cs
@@ -0,0 +1,76 @@
+﻿using BookingApp.Entities.Models;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Configuration;
+
+namespace BookingApp.Persistence.Repositories
+{
+    public class RoomRepository
+    {
+        private readonly string _connectionString;
+        public RoomRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration?.GetConnectionString("DefaultConnection") ?? throw new ArgumentNullException(nameof(configuration));
+        }
+        public void Add(Room room)
+        {
+            using (var connection = new SqliteConnection(_connectionString))
+            {
+                connection.Open();
+                CreateTableIfNotExists(connection);
+                SqliteCommand command = new SqliteCommand($"INSERT INTO Rooms(Id, Num SquareMeters, Capacity, NumberOfBeds, Floor, PricePerNight) VALUES('{room.Id}', {room.SquareMeters}, {room.Capacity}, {room.NumberOfBeds}, {room.Floor}, {room.PricePerNight})", connection);
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+        public Room GetById(string id)
+        {
+            Room room = new();
+
+            using (var connection = new SqliteConnection(_connectionString))
+            {
+                connection.Open();
+
+                //create table if not exists
+                CreateTableIfNotExists(connection);
+
+                string sql = $"SELECT Id, Num, SquareMeters, Capacity, NumberOfBeds, Floor, PricePerNight FROM Rooms WHERE Id = '{id}'";
+                SqliteCommand command = new SqliteCommand(sql, connection);
+                SqliteDataReader dataReader = command.ExecuteReader();
+
+                if (dataReader.Read())
+                {
+                    room.Id = dataReader.GetString(0);
+                    room.Num = dataReader.GetInt32(1);
+                    room.SquareMeters = dataReader.GetDouble(2);
+                    room.Capacity = dataReader.GetInt32(3);
+                    room.NumberOfBeds = dataReader.GetInt32(4);
+                    room.Floor = dataReader.GetInt32(5);
+                    room.PricePerNight = dataReader.GetDouble(6);
+                }
+
+                connection.Close();
+            }
+
+            return room;
+        }
+
+        public void CreateRoom(Room room)
+        {
+            using (var connection = new SqliteConnection(_connectionString))
+            {
+                connection.Open();
+
+                CreateTableIfNotExists(connection);
+
+                connection.Close();
+            }
+        }
+        private void CreateTableIfNotExists(SqliteConnection connection)
+        {
+            //create table if not exists
+            string createTableQuery = "CREATE TABLE IF NOT EXISTS Rooms (Id TEXT PRIMARY KEY, TEXT, Num INTEGER, SquareMeters REAL, Capacity INTEGER, NumberOfBeds INTEGER, Floor INTEGER, PricePerNight REAL)";
+            SqliteCommand createCommand = new SqliteCommand(createTableQuery, connection);
+            createCommand.ExecuteNonQuery();
+        }
+    }
+}
